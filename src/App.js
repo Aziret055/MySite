635dbac9581caa6353f5c9533dd854fa10425862
Aziret055 <kojom5erd1ev1@gmail.com>
2024-04-14 12:58:28 +0600

    finally

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import About from './components/About';
-import Blocks from './components/Blocks';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Reviews from './components/Reviews';
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import About from './components/About'
+import Blocks from './components/Blocks'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Reviews from './components/Reviews'
+import gsap from 'gsap'
+import { ScrollToPlugin } from 'gsap/all'
+gsap.registerPlugin(ScrollToPlugin)
+
 
 function App() {
-  return (
-    <div className="App">
-<Header/>
-<Hero/>
-<Blocks/>
-<About/>
-<Reviews/>
-<Routes>
-  <Route path='/footer' element={ <Footer/> }/>
-</Routes>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Header />
+			<Hero />
+			<Blocks />
+			<About />
+			<Reviews />
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
